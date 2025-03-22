1ad91a65a13156ace314f1b9c5f53334efcaae84
unknown <moazzemh222@gmail.com>
2025-03-22 12:07:56 +0600

    Console Calculator and Guassain game

---
+++ Console App/Calculator/Program.cs
@@ -0,0 +1,64 @@
+﻿class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Do you want to continue? (y/n): ");
+            string input = Console.ReadLine()?.ToLower();
+
+            if(input == "n")
+            {
+                Console.WriteLine("Exiting the process...");
+                break;
+            }
+            else if(input == "y")
+            {
+                Console.WriteLine("Enter First Number.");
+                double num1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Second Number.");
+                double num2 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Operator (+, -, *, /, %)");
+                char op = Console.ReadKey().KeyChar;
+
+                double result;
+
+                switch (op)
+                {
+                    case '+':
+                        result = num1 + num2;
+                        Console.WriteLine("\nThe result is: " + result);
+                        break;
+
+                    case '-':
+                        result = num1 - num2;
+                        Console.WriteLine("\nThe result is: " + result);
+                        break;
+
+                    case '*':
+                        result = num1 * num2;
+                        Console.WriteLine("\nThe result is: " + result);
+                        break;
+
+                    case '/':
+                        result = num1 / num2;
+                        Console.WriteLine("\nThe result is: " + result);
+                        break;
+
+                    case '%':
+                        result = num1 % num2;
+                        Console.WriteLine("\nThe result is: " + result);
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("invalid input! please type 'y' to continue or type 'n' to break the process.");
+            }
+        }
+
+       
+    }
+}
---
+++ Console App/Guassain Game/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Write("Do you want to continue? (y/n): ");
+            string input = Console.ReadLine()?.ToLower();
+
+            if (input == "n")
+            {
+                Console.WriteLine("Exiting the process...");
+                break; 
+            }
+            else if (input == "y")
+            {
+
+                Random rd = new Random();
+                int rand_num = rd.Next(1, 10);
+
+                for (int i=1; i<=5; i++)
+                {
+
+                    Console.WriteLine("The random number is: " + rand_num);
+
+                    Console.WriteLine("\nEnter a value between 1 to 10");
+                    int number = int.Parse(Console.ReadLine());
+
+                    if (number == rand_num)
+                    {
+                        
+                        Console.WriteLine("Congratulation!!! You guass is right.\n");
+                        Console.WriteLine("-----------------------------------------\n");
+                        break;
+                    }
+                    else
+                    {
+                        int trial = 5 - i;
+
+                        if (trial > 0)
+                            Console.WriteLine("You loss! You can try " + trial + " more time.");
+                        else
+                            Console.WriteLine("You lose!!! Now you recharge 10 taka on this number 01900000.");
+                    }
+                }
+               
+            }
+            else
+            {
+                Console.WriteLine("Invalid input! Please enter 'y' to continue or 'n' to exit.");
+            }
+        }
+    }
+}

29bbc4c8f09c1ddff358ba32fef4edeccd1c218d
unknown <moazzemh222@gmail.com>
2025-03-24 12:09:24 +0600

    MethodOverloading1 in OOP

---
+++ OOP/Describe in OOP/ConsoleApp2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+//Use Top level Statement, when I don't use the main method
+//var AC = new Car();
+//AC.Accelerate();
+//AC.Break();
+
+public class Car
+{
+    private int speed = 0;
+
+    public int Speed
+    {
+        get { return speed; }
+    }
+
+    public void Accelerate()
+    {
+        if (speed < 200)
+        {
+            speed += 10;
+            Console.WriteLine($"Car accelerated. Speed: {speed} km/h");
+        }
+        else
+        {
+            Console.WriteLine("Maximum speed reached!");
+        }
+    }
+
+    public void Break()
+    {
+        if(speed > 0)
+        {
+            speed -= 10;
+            Console.WriteLine($"Car slower down Speed: {speed} km/h");
+        }
+        else
+        {
+            Console.WriteLine("Car is already stopped.");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Car myCar = new Car();
+        myCar.Accelerate();
+        myCar.Break();
+        Console.WriteLine($"Current Speed: {myCar.Speed} km/h");
+    }
+}
+
+
+
+
---
+++ OOP/Describe in OOP/MethodOverloading1/Program.cs
@@ -0,0 +1,50 @@
+﻿
+//Top level Statement, when I don't use main method
+/* var MP = new MusicPlayer();
+MP.Play();
+
+var MP3 = new Mp3Player();
+MP3.Play();
+
+var Wav = new WavPlayer();
+Wav.Play(); */
+
+public class MusicPlayer
+{
+    public virtual void Play()
+    {
+        Console.WriteLine("Playing Music");
+    }
+}
+
+public class Mp3Player : MusicPlayer
+{
+    public override void Play()
+    {
+        Console.WriteLine("Mp3 Music play.");
+    }
+}
+
+public class WavPlayer : MusicPlayer
+{
+    public override void Play()
+    {
+        Console.WriteLine("Wav Music Play.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        MusicPlayer player = new MusicPlayer();
+        player.Play();
+
+        MusicPlayer player1 = new Mp3Player();
+        player1.Play();
+
+        MusicPlayer player2 = new WavPlayer();
+        player2.Play();
+
+    }
+}

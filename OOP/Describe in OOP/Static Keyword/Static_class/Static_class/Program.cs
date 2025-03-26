309c807f21adb0b444e1cf96f71394c8373cf3f2
unknown <moazzemh222@gmail.com>
2025-03-26 12:33:07 +0600

    Added Static Keyword folder

---
+++ OOP/Describe in OOP/Static Keyword/Static_class/Static_class/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+static class Tutorial
+{
+    public static string Topics = "Static class";
+}
+
+public class Movie
+{
+     public string msg = "Not static class";
+}
+
+class program
+{
+    static void Main()
+    {
+        Movie movie = new Movie();
+        
+        Console.WriteLine("Topics name is: {0}", Tutorial.Topics);
+        Console.WriteLine("Topics Name is: {0}", movie.msg);
+    }
+}

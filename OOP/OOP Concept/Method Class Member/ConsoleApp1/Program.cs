90b9d63ec88b458a80a58ae2af99f28a28d42069
unknown <moazzemh222@gmail.com>
2025-04-06 17:22:21 +0600

    Encapsulation and Getter & Setter

---
+++ OOP/OOP Concept/Method Class Member/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿class Person
+{
+    public string name;
+
+    protected int age;
+
+    public void SetAge(int a)
+    {
+        age = a;
+    }
+
+    private void SetAge2(int a)
+    {
+        age = a;
+    }
+
+    public int GetAge()
+    {
+        SetAge2(2);
+        return age;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {name}, Age: {age}");
+    }
+}
+
+class Test
+{
+    public static void Main(string[] args)
+    {
+        Person p1 = new Person();
+        p1.name = "Arif";
+        
+        p1.SetAge(33);
+        Console.WriteLine($"Name: {p1.name}, Age: {p1.GetAge()}");
+        Console.WriteLine($"Age: {p1.GetAge()}");
+    }
+}
---
+++ OOP/OOP Concept/Method Class Member/Constructor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+class Person
+{
+    public string name;
+    public int age;
+
+    public Person()  //Default Constructor
+    {
+        name = "Rahim";
+        age = 33;
+    }
+
+    public Person(string n, int a) //Paramiterise Constructor
+    {
+        name = n;
+        age = a;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {name}, Age: {age}");
+    }
+
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Person p1 = new Person();
+        p1.DisplayInfo();
+
+        Person p2 = new Person("Sakib", 32);
+        p2.DisplayInfo();
+
+        Person p3 = new Person("Rakib", 34);
+        p2.DisplayInfo();
+    }
+}

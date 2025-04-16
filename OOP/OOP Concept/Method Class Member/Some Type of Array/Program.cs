396dc8f1450f526f65649106f08160bae664def9
unknown <moazzemh222@gmail.com>
2025-04-16 21:51:32 +0600

    3 type of output list using for loop

--- OOP/OOP Concept/Method Class Member/Some Type of Array/Program.cs
+++ OOP/OOP Concept/Method Class Member/Some Type of Array/Program.cs
@@ -1,26 +1,69 @@
-﻿class Program
+﻿//class Program
+//{
+//    static void Main()
+//    {
+//        Console.Write("Enter how many numbers you want to input: ");
+//        int count = int.Parse(Console.ReadLine());
+//        int[] numbers = new int[count];
+//        for (int i = 0; i < count; i++)
+//        {
+//            Console.Write($"Enter number {i + 1}: ");
+//            numbers[i] = int.Parse(Console.ReadLine());
+//        }
+//        foreach (int num in numbers)
+//        //for (int i = 0; i < count; i++)
+//        {
+//            Console.WriteLine($"The number is: {num}");
+//            //Console.WriteLine($"Inputed number is {i + 1}: {numbers[i]}");
+//        }
+//    }
+//}
+
+class Program
 {
     static void Main()
     {
-        Console.Write("Enter how many numbers you want to input: ");
+        Console.Write("How many numbers do you want to input:  ");
         int count = int.Parse(Console.ReadLine());
 
         int[] numbers = new int[count];
 
-
-        for (int i = 0; i < count; i++)
+        for (int i=0; i<count; i++)
         {
-            Console.Write($"Enter number {i + 1}: ");
+            Console.Write($"Num {i + 1}: ");
             numbers[i] = int.Parse(Console.ReadLine());
         }
 
-        foreach (int num in numbers)
-        //for (int i = 0; i < count; i++)
+        Console.Write("\n");
+        Console.Write($"\nList of numbers devided by 3: ");
+        for (int i = 0; i < count; i++)
         {
-            Console.WriteLine($"The number is: {num}");
-            //Console.WriteLine($"Inputed number is {i + 1}: {numbers[i]}");
+            //Console.WriteLine($"Num {i + 1}: {numbers[i]}");
+            if (numbers[i]%3 == 0)
+            {
+                Console.Write($"\n {numbers[i]}"); 
+            }
         }
 
+        Console.Write("\n");
+        Console.Write($"\nList of numbers devided by 5: ");
+        for (int i = 0; i < count; i++)
+        {
+            if (numbers[i] % 5 == 0)
+            {
+                Console.Write($"\n {numbers[i]}");
+            }
+        }
 
+        Console.Write("\n");
+        Console.Write($"\nAnother numbers are: ");
+        for (int i = 0; i < count; i++)
+        {
+            if (numbers[i] % 3 != 0 && numbers[i] % 5 != 0)
+            {
+                Console.Write($"\n {numbers[i]}");
+            }
+        }
+        Console.Write("\n\n");
     }
 }
--- OOP/OOP Concept/Method Class Member/Some Type of Array/obj/Debug/net9.0/Some Type of Array.AssemblyInfo.cs
+++ OOP/OOP Concept/Method Class Member/Some Type of Array/obj/Debug/net9.0/Some Type of Array.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Some Type of Array")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b52bef4ac21243cdb1e196d80f3a7c4064e8c15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68149d5ebe2d07bf5cfbfda13295d41093097de1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Some Type of Array")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Some Type of Array")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

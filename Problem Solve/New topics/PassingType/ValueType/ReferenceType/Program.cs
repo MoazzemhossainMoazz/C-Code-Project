f0830cf512677978b8c7672dafdac6b2ea6aea92
unknown <moazzemh222@gmail.com>
2025-04-26 23:55:54 +0600

    Passing Type

---
+++ Problem Solve/New topics/PassingType/ValueType/ReferenceType/Program.cs
@@ -0,0 +1,25 @@
+﻿
+class Program
+{
+    static void ChangeReferenceType(Student std2)
+    {
+        std2.studentName = "Steve";
+        Console.WriteLine(std2.studentName);
+    }
+
+    static void Main(string[] args)
+    {
+        Student std1 = new Student();
+        ChangeReferenceType(std1);
+        std1.studentName = "Bill";
+        Console.WriteLine(std1.studentName);
+        
+        ChangeReferenceType(std1);
+        Console.WriteLine(std1.studentName);
+    }
+}
+
+public class Student
+{
+    public string studentName;
+}

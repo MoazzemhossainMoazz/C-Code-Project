f0531c113590781012d82d946faedb66d69c9135
unknown <moazzemh222@gmail.com>
2025-06-09 23:33:10 +0600

    Virtual Property

---
+++ Keywords/Virtual/VirtualProperty/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Person
+{
+    public virtual string Name { get; set; } = "Unnamed";
+
+}
+public class Student: Person
+{
+    public override string Name { get; set; } = "Student";
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Person p = new Person();
+        Student s = new Student();
+
+        Console.WriteLine(p.Name);
+        Console.WriteLine(s.Name);
+    }
+}

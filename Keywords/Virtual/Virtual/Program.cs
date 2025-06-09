b850ffa7d7d0be325bfd6f3af6f41d9a837a3206
unknown <moazzemh222@gmail.com>
2025-06-09 23:04:38 +0600

    Virtual keyword

---
+++ Keywords/Virtual/Virtual/Program.cs
@@ -0,0 +1,28 @@
+﻿public class BaseClass
+{
+    public virtual void Greet()
+    {
+        Console.WriteLine("Hello from BaseClass");
+    }
+}
+public class DerivedClass : BaseClass
+{
+    public override void Greet()
+    {
+        base.Greet();
+        Console.WriteLine("Hello from Derivedclass");
+    }
+}
+
+class Program()
+{
+    public static void Main()
+    {
+        BaseClass baseClass = new BaseClass();
+        baseClass.Greet();
+
+        DerivedClass derivedclass = new DerivedClass();
+        derivedclass.Greet();
+
+    }
+}

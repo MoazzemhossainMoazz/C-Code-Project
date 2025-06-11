7a3ef1106e9a251892f873c121fcb735ee280d25
unknown <moazzemh222@gmail.com>
2025-06-11 12:53:57 +0600

    Override

---
+++ Keywords/Override/Override/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Person
+{
+    public virtual string Role => "Person";
+}
+
+public class Student : Person
+{
+    public override string Role => "Student";
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Person p = new Person();
+        Console.WriteLine(p.Role);
+
+        Student s = new Student();
+        Console.WriteLine(s.Role);
+    }
+}

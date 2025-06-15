81d5fad6296808d946865deaebe7375566b99362
unknown <moazzemh222@gmail.com>
2025-06-15 16:29:28 +0600

    Proj Simple Login System

---
+++ Keywords/Sealed/Proj Simple Login System/Program.cs
@@ -0,0 +1,35 @@
+﻿public sealed class AuthManager
+{
+    private string storedUsername = "admin";
+    private string storedPassword = "1234";
+
+    public bool Login(string username, string password)
+    {
+        if (username == storedUsername && password == storedPassword)
+        {
+            Console.WriteLine("Login Successfull");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Invalid Username or Password");
+            return false;
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        AuthManager auth = new AuthManager();
+
+        Console.Write("Username: ");
+        string? user = Console.ReadLine();
+
+        Console.Write("Password: ");
+        string? pass = Console.ReadLine();
+
+        auth.Login(user ?? "", pass ?? "");
+    }
+}

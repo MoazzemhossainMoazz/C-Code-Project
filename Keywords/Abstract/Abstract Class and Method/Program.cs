a60512c254de7d3731879349f8b8770bdd2daa2e
unknown <moazzemh222@gmail.com>
2025-06-16 16:37:22 +0600

    Abstract class and method

---
+++ Keywords/Abstract/Abstract Class and Method/Program.cs
@@ -0,0 +1,38 @@
+﻿
+//public abstract class
+public class Animal
+{
+
+    //public abstract void MakeSound();
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Animal sound");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("Eating...");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Woof!");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Animal animal = new Animal();
+        animal.MakeSound();
+        animal.Eat();
+
+        Dog dog = new Dog();
+        dog.MakeSound();
+        dog.Eat();
+    }
+}
---
+++ Keywords/Abstract/Abstract/Program.cs
@@ -0,0 +1,27 @@
+﻿public abstract class Animal
+{
+    public abstract void MakeSound();
+
+    public void Eat()
+    {
+        Console.WriteLine("Eating...");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Woof! Woof!");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Dog dog = new Dog();
+        dog.MakeSound();
+        dog.Eat();
+    }
+}

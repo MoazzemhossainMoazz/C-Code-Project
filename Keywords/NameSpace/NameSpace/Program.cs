0659a428cdf4bf523b91bdf838b949295e6de3ff
unknown <moazzemh222@gmail.com>
2025-06-16 23:52:49 +0600

    NameSpace

---
+++ Keywords/NameSpace/NameSpace/Program.cs
@@ -0,0 +1,41 @@
+﻿using MyApp.Display;
+using MyApp.Operations;
+
+namespace MyApp.Operations
+{
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+
+        public int Subtract(int a, int b) => a - b;
+    }
+}
+
+namespace MyApp.Display
+{
+    public class Printer
+    {
+        public void Show(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
+
+
+public class Program
+{
+    public static void Main()
+    {
+        Calculator calc = new Calculator();
+        Printer printer = new Printer();
+
+        int sum = calc.Add(10, 5);
+        int difference = calc.Subtract(10, 5);
+
+        printer.Show($"Sum: {sum}");
+        printer.Show($"Difference: {difference}");
+    }
+}
+
+

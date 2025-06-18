fe57d088ec7cfb2566cf388b26166ac935a918ee
unknown <moazzemh222@gmail.com>
2025-06-18 21:30:13 +0600

    Const Keyword

---
+++ Keywords/Const Keyword/Const Keyword/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Circle
+{
+    private const double PI = 3.1416;
+
+    public double CalculateArea(double radius)
+    {
+        return PI * radius * radius;
+    }
+
+    public static void Main()
+    {
+        Circle circle = new Circle();
+        double c = circle.CalculateArea(4.5);
+        Console.WriteLine("Area is: " + c);
+    }
+}

422f448ef5e60ca8a7b552b5ea4fc654e96fbdef
unknown <moazzemh222@gmail.com>
2025-06-18 20:50:14 +0600

    Null Keyword

---
+++ Keywords/Null Keyword/Null Keyword/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        string? message = null;
+
+        if (message == null)
+            Console.WriteLine("Message is null.");
+        else
+            Console.WriteLine("Message" + message);
+
+        int? value = null;
+        Console.WriteLine("Value is zero: " + (value ?? 0));
+    }
+}

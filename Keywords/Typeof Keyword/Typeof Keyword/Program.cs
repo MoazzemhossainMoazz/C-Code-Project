5b5eeb4a3baab95d166d183c4d1265beba9a4f4d
unknown <moazzemh222@gmail.com>
2025-06-19 23:21:28 +0600

    Typeof Keyword

---
+++ Keywords/Typeof Keyword/Typeof Keyword/Program.cs
@@ -0,0 +1,30 @@
+﻿//class Program
+//{
+//    static void Main()
+//    {
+//        Type typeInfo = typeof(int);
+//        Console.WriteLine(typeInfo.FullName);
+//    }
+//}
+
+
+public class Person
+{
+    public string Name;
+    public int Age;
+}
+
+class Program
+{
+    static void Main()
+    {
+        Type t = typeof(Person);
+
+        Console.WriteLine("Type Name: " + t.Name);
+
+        foreach(var field in t.GetFields())
+        {
+            Console.WriteLine("Field: " + field.Name + "(" + field.FieldType + ")");
+        }
+    }
+}

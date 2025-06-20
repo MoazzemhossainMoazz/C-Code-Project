6b72abba2f7fcd1a3d7bbb10eb46089de639334f
unknown <moazzemh222@gmail.com>
2025-06-20 23:58:33 +0600

    Is keyword

---
+++ Keywords/Is keyword/Is keyword/Program.cs
@@ -0,0 +1,25 @@
+﻿class Animal { }
+class Cat : Animal { }
+class Program
+{
+    public static void Main()
+    {
+        Animal pet = new Cat();
+
+        object obj = 42.2;
+
+        if(obj is int number)
+        {
+            Console.WriteLine("Number " + number + " integer number.");
+        }
+        //else
+        //{
+        //    Console.WriteLine("Number " + number + " float number.");
+        //}
+
+        if(pet is Cat)
+        {
+            Console.WriteLine("Pet is Cat");
+        }
+    }
+}

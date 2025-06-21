6dce48c90c655b781b394575e46a420825a97064
unknown <moazzemh222@gmail.com>
2025-06-21 23:09:15 +0600

    as Keyword

---
+++ Keywords/as Keyword/as Keyword/Program.cs
@@ -0,0 +1,19 @@
+﻿class Animal { }
+class Dog : Animal
+{
+    public void Bark() => Console.WriteLine("Woof!");
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Animal pet = new Dog();
+        Dog d = pet as Dog;
+
+        if (d != null)
+        {
+            d.Bark();
+        }
+    }
+}

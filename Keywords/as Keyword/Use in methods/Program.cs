57ef6e91ac5138b0031435d86d2bf652a264b1bb
unknown <moazzemh222@gmail.com>
2025-06-23 23:52:08 +0600

    As keyword use method

---
+++ Keywords/as Keyword/Use in methods/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+class Program
+{
+    static void Main()
+    {
+        void Speak(object obj)
+        {
+            string s = obj as string;
+
+            if (s != null)
+            {
+                Console.WriteLine("You sain: " + s);
+            }
+            else
+            {
+                Console.WriteLine("Not a string.");
+            }
+
+        }
+
+        Speak("Hi there");
+        Speak(123);
+    }
+}
